eb7b7ca9065072df49666c6f3d60a029f57d4c45
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-21 09:48:35 -0500

    Messaging: complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -12,6 +12,7 @@ class Kele
 		response = self.class.post(@bloc_base_api_url, body: {email: username, password: password})
 
 		@user_auth_token = response["auth_token"]
+		@user_email = username
 
 		puts 'Invalid credentials' if @user_auth_token.nil?
 	end
@@ -24,5 +25,28 @@ class Kele
 	def get_mentor_availability
 		response = self.class.get("https://www.bloc.io/api/v1/mentors/2299934/student_availability", headers: { "authorization" => @user_auth_token })
 		@my_mentor_avilability = JSON.parse(response.body)
+	end
+
+	def get_messages(page = 0)
+		if page > 0
+			response = self.class.get("https://www.bloc.io/api/v1/message_threads?page=#{page}", headers: { "authorization" => @user_auth_token })
+		else
+			response = self.class.get("https://www.bloc.io/api/v1/message_threads", headers: { "authorization" => @user_auth_token })
+		end
+
+		@my_message_threads = JSON.parse(response.body)['items']
+	end
+
+	def create_message(recipient_id, message_thread_token, subject, body)
+		response = self.class.post("https://www.bloc.io/api/v1/messages",
+			body: {
+				sender: @user_email,
+				recipient_id: recipient_id,
+				token: message_thread_token,
+				subject: subject,
+				"stripped-text": body
+				})
+
+		response ? "Message sent" : "Something went wrong"
 	end
 end
